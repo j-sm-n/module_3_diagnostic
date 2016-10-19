c847bd147eefc32ffc0aedcd62eba740cf4d70cc
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-19 09:48:27 -0600

    Add first byebug in controller to test out params and farday

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
-
+    byebug
+    response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=80203&radius=6.0&fuel_type=ELEC,LPG&limit=10"
   end
+
+  # def conn
+  #   Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json') do |faraday|
+  #     faraday.adapter  Faraday.default_adapter
+  #
+  #   end
+  # end
 end

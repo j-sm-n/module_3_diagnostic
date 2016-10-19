c12f2c548e625f88e28b727c12a454e3f177f4a4
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-19 09:52:55 -0600

    Add VCR to feature test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{params['q']}&radius=6.0&fuel_type=ELEC,LPG&limit=10"
     byebug
-    response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=80203&radius=6.0&fuel_type=ELEC,LPG&limit=10"
   end
 
   # def conn

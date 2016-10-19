74e341ecd72aa229641d25cbf088e10ef255438a
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-19 10:40:20 -0600

    Fix unit test for station

--- spec/models/fuel_station_spec.rb
+++ spec/models/fuel_station_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-descirbe "Fuel Station Model" do
+describe "Fuel Station Model" do
   it "has all attributes of a station" do
     station = FuelStation.new(station_name: "Station1",
                               street_address: "770 Grant St.",
                               fuel_type_code: "ELEC",
                               distance: "1.5",
-                              access_times: "24 hours a day")
+                              access_days_time: "24 hours a day")
 
     expect(station.name).to eq("Station1")
     expect(station.address).to eq("770 Grant St.")
